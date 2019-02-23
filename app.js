7d8ddc4005730680b9fc7c4fbad885bef904514e
tiravat <tiravat2016@gmail.com>
2019-02-23 13:11:29 +0700

    :sparkles: Created a mechanism to display modals

--- app.js
+++ app.js
@@ -7,8 +7,16 @@ function toggleSideNavigationMenu() {
       : "nav-menu-background-open";
 
   const menuButton = document.getElementById("navbar-menu-button");
-  menuButton.innerHTML =  targetClassName === "nav-menu-background-open"
-      ? "MENU"
-      : "CLOSE";
+  menuButton.innerHTML =
+    targetClassName === "nav-menu-background-open" ? "MENU" : "CLOSE";
   return;
+}
+
+function toggleModal() {
+  const target = document.getElementById("subscribeModal");
+  const targetClassName = target.className;
+  target.className =
+    targetClassName === "modal-background-open"
+      ? "modal-background-close"
+      : "modal-background-open";
 }
