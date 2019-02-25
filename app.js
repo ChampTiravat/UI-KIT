259ca4737f9b7151ab0819a665584fba4bca99bb
tiravat <tiravat2016@gmail.com>
2019-02-25 20:38:56 +0700

    :hammer: Added some spaces in each lines

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function toggleSideNavigationMenu() {
   const target = document.getElementById("sideNavigationMenu");
   const targetClassName = target.className;
+
   target.className =
     targetClassName === "nav-menu-background-open"
       ? "nav-menu-background-close"
@@ -9,12 +10,14 @@ function toggleSideNavigationMenu() {
   const menuButton = document.getElementById("navbar-menu-button");
   menuButton.innerHTML =
     targetClassName === "nav-menu-background-open" ? "MENU" : "CLOSE";
+
   return;
 }
 
 function toggleModal() {
   const target = document.getElementById("subscribeModal");
   const targetClassName = target.className;
+
   target.className =
     targetClassName === "modal-background-open"
       ? "modal-background-close"
